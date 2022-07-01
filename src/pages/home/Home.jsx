771d2d9d195d41dae2300ddf68af2599264e2238
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-07-01 20:01:36 +0530

    search bar UI done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,14 @@ export const Home = () => {
     return (
         <>
             <div className={style.main}>
-                
+                <input
+                    type={'text'}
+                    className={style.input}
+                    placeholder='type username here'
+                />
+                <button
+                    className={style.btn}
+                >Search</button>
             </div>
         </>
     );
