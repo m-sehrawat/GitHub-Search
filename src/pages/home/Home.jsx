03aa067668c62797aeba24738008286c1f389e9d
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-07-01 20:40:33 +0530

    repo data coming from server

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,23 +1,34 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { shallowEqual } from 'react-redux';
 import { useSelector } from 'react-redux';
+import { getDataRequest } from '../../redux/action';
 import style from './home.module.css'
 
 export const Home = () => {
 
     const { isLoading, isError, repoData } = useSelector((state) => state, shallowEqual);
+    console.log('repoData:', repoData)
+    const [username, setUsername] = useState('');
+    const dispatch = useDispatch();
 
 
+    const handleGetData = () => {
+        dispatch(getDataRequest(username));
+    };
 
 
     return (
         <>
             <div className={style.main}>
                 <input
+                    onChange={(e) => { setUsername(e.target.value) }}
                     type={'text'}
                     className={style.input}
                     placeholder='type username here'
                 />
                 <button
+                    onClick={handleGetData}
                     className={style.btn}
                 >Search</button>
             </div>
