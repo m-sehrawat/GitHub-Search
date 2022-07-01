767c6dada4fd26221c92fc3ef46cee1025d4bf6a
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-07-02 00:21:32 +0530

    Link to repo details added

--- src/components/RepoComponent.jsx
+++ src/components/RepoComponent.jsx
@@ -1,17 +1,22 @@
-export const RepoComponent = ({ style, owner, name, description }) => {
+import { Link } from "react-router-dom";
+
+export const RepoComponent = ({ style, owner, name, description, id }) => {
     return (
         <>
-            <div className={style.gridItem}>
+            <Link className={style.link} to={`/details/${id}`}>
+                
+                <div className={`${style.gridItem} ${style.cursor}`}>
 
-                <div className={style.imgBox}>
-                    <img src={owner.avatar_url} />
-                </div>
+                    <div className={style.imgBox}>
+                        <img src={owner.avatar_url} />
+                    </div>
 
-                <div className={style.textBox}>
-                    <h4>{name}</h4>
-                    <p>{description}</p>
+                    <div className={style.textBox}>
+                        <h4>{name}</h4>
+                        <p>{description}</p>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </>
-    );
+    )
 };
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,13 +1,15 @@
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Home } from "../pages/home/Home";
+import { RepoDetails } from "../pages/repoDetails/RepoDetails";
+
 
 export const Router = () => {
-
     return (
         <>
             <Routes>
                 <Route path={'/'} element={<Home />} />
+                <Route path={'/details/:id'} element={<RepoDetails />} />
             </Routes>
         </>
     );
