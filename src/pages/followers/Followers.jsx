13e783fd0c73ebc8d0e88ace7a6d6d8e21af36e1
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-07-02 10:45:35 +0530

    followers page layout added

--- src/pages/followers/Followers.jsx
+++ src/pages/followers/Followers.jsx
@@ -1,8 +1,20 @@
+import style from './followers.module.css';
+
 export const Followers = () => {
 
     return (
         <>
-            <h1>Followers</h1>
+            <div className={style.main}>
+                <div className={style.textBox}>
+                    <h3>Followers</h3>
+                </div>
+
+                <div className={style.grid}>
+                    {[[1, 2, 3, 4].map((follower) => (
+                        <div>1</div>
+                    ))]}
+                </div>
+            </div>
         </>
     );
 };
